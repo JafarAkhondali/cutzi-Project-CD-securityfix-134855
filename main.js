d92669b16794050385c64d6a1c899131ca565287
Bananenplantage <dominik.kura@web.de>
2015-09-02 11:34:14 +0200

    Unnötige Zeilen gelöscht
    Auskommentierte Zeile, welche nicht mehr relevant sind, wurden entfernt.

--- main.js
+++ main.js
@@ -50,31 +50,3 @@
 				renderer.render(scene,camera);
 			}
 			render();
-			
-			/*function onDocumentKeyDown(event){
-				var keyCode = event.which;
-				
-				//up
-				if(keyCode == 38){
-					cube.translateZ(-0.1);
-					camera.translateZ(-0.1);
-				}
-				
-				//down
-				if(keyCode == 40){
-					cube.translateZ(0.1);
-					camera.translateZ(0.1);
-				}
-
-				//left
-				if(keyCode == 37){
-					cube.translateX(-0.1);
-					camera.translateX(-0.1);
-				}
-				
-				//right
-				if(keyCode == 39){
-					cube.translateX(0.1);
-					camera.translateX(0.1);
-				}
-			}*/
