101ca74d12fce3255da6c710bf0a8c7678c0a0c3
Bananenplantage <dominik.kura@web.de>
2016-01-25 11:18:44 +0100

    Changed text in endstate

--- endstate.js
+++ endstate.js
@@ -8,10 +8,10 @@ function switchToEndstate(){
 function showGameoverText(){
 	clearTextMessage();
 	if(hpCounter <= 0){
-		appendTextMessage("Du bist tot :( <br> Du hast leider all deine Lebenspunkte verloren.<br> Mit [R] kannst du es nochmal versuchen!");
+		appendTextMessage("Du bist tot :( <br> Du hast leider all deine Lebenspunkte verloren.<br> Aber Hey! Mit [R] kannst du es nochmal versuchen!");
 	}
 	else{
-		appendTextMessage("Du hast zu lange gebraucht und es ist dunkel geworden.");
+		appendTextMessage("Du hast zu lange gebraucht und es ist dunkel geworden. <br> Du findest nicht mehr nach Hause und verhungerst im Wald. :( <br> Aber Hey! Mit [R] kannst du es nochmal versuchen!");
 	}
 }
 
